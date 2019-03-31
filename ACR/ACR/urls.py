5499a843ba70f1d698e75b760e2c3f1565e31e0d
Shubham <sharma.shubham736@gmail.com>
2019-03-31 19:30:15 +0530

    Added auth

--- ACR/ACR/urls.py
+++ ACR/ACR/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('acrblockchain/', include('ACRBlockchain.urls')),
+    path('acrblockchain/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    # path('', TemplateView.as_view(template_name='registration/login.html'), name='home'),
 ]
